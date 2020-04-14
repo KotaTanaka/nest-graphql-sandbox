f5cbeab6e1a02aab440d901cb657559eff1c0640
KotaTanaka <te11k.tanaka@gmail.com>
2020-04-14 18:42:15 +0900

    refactor: GraphQLの型明示

--- src/domain/books/dto/create-book-body.dto.ts
+++ src/domain/books/dto/create-book-body.dto.ts
@@ -2,11 +2,11 @@ import { InputType, Field } from '@nestjs/graphql';
 
 @InputType()
 export class CreateBookBody {
-  @Field() readonly name: string;
-  @Field() readonly outline: string;
-  @Field() readonly author: string;
-  @Field() readonly publisher: string;
-  @Field() readonly category: string;
-  @Field() readonly price: number;
-  @Field() readonly releasedAt: string;
+  @Field(() => String) readonly name: string;
+  @Field(() => String) readonly outline: string;
+  @Field(() => String) readonly author: string;
+  @Field(() => String) readonly publisher: string;
+  @Field(() => String) readonly category: string;
+  @Field(() => Number) readonly price: number;
+  @Field(() => String) readonly releasedAt: string;
 }
